baae0153d351821a31598fbcd40858abda06a376
Rohan Katkam <157429658+rk94407@users.noreply.github.com>
2025-10-01 00:45:46 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ["pdf-"]
+  serverExternalPackages: ["pdf-"],
+  devIndicators: false,
 };
 
 export default nextConfig;
