dab6f60b5d2818f9b7cdb7482a8e53cc2cde832e
Rohan Katkam <157429658+rk94407@users.noreply.github.com>
2025-10-01 12:04:07 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className="flex flex-col h-screen items-center justify-center">
+        <h1 className="text-4xl font-bold">Welcome to RAG AI ChatBot</h1>
+        <p className="text-muted-foreground">Powered by OpenAI</p>
+      </div>
+    </>
+  );
 }
